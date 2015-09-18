c38bfbe879e089ede27758c3ef1fadeea7a1a8de
Anderson Tao <andersontao@macbook-air.attlocal.net>
2015-09-17 21:53:03 -0700

    change black assasin color to grey to simplify code

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ var sessionData = [];
 var COLOR_RED = "#ff0000";
 var COLOR_YELLOW = "#ffff00";
 var COLOR_BLUE = "#00eeee";
-var COLOR_BLACK = "black";
+var COLOR_BLACK = "#808080";
 var COLOR_GREEN = "#009000";
 
 function fire(){
