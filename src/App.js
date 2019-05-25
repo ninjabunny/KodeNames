e2faa0c883c6fbd56b8a90dae006fa5843c6dc14
Andee Tao <andeetao@Andees-iMac.attlocal.net>
2019-05-25 11:53:23 -0700

    fixed maximize size

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import Card from '@material-ui/core/Card'
 
 const useStyles = makeStyles(theme => ({
   root: {
+    // height: '10px',
     flexGrow: 1,
   },
   menuButton: {
@@ -24,22 +26,33 @@ function App() {
   const classes = useStyles()
   return (
     <div className='App'>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='Menu'
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant='h6' className={classes.title}>
-            News
-          </Typography>
-          <Button color='inherit'>Login</Button>
-        </Toolbar>
-      </AppBar>
+      <div className='appBar'>
+        <AppBar position='static'>
+          <Toolbar variant='dense'>
+            <IconButton
+              edge='start'
+              className={classes.menuButton}
+              color='inherit'
+              aria-label='Menu'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' className={classes.title}>
+              KodeNames
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <table className='table'>
+        {new Array(5).fill(1).map(_ => (
+          <tr>
+            {new Array(5).fill(1).map(_ => (
+              <td className='card'> hi</td>
+            ))}
+          </tr>
+        ))}
+      </table>
     </div>
   )
 }
